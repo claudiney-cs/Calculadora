8a262060c573637cc67eb4a0ccca80fda9821cf3
Claudineycs <claudineys_santos@hotmail.com>
2021-11-15 19:27:48 -0300

    Adicionado .eslintric.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,32 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    plugins: [
+        "react", 
+        "prettier"
+      ],
+        rules: {
+          "prettier/prettier": "error",
+          "react/jsx-filename-extension": [
+          "warn", { 
+            extensions: [".jsx", ".js"] 
+          }
+        ],
+        "import/prefer-default-export": "off",
+        "no-param-reassign": "off",
+        "no-console": ["error", { allow: ["tron"] }]
+        }
+      } 
