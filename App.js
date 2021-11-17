0cc88489c62ffa4f684c935dc7f35acd4b7bf38b
Claudineycs <claudineys_santos@hotmail.com>
2021-11-16 22:09:23 -0300

    Melhorias do aplicativo

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View, TouchableOpacity, Button, LogBox } from 'react-
 export default function App() {
   LogBox.ignoreLogs(['Remote debugger']);
   // Mapeamento de teclas
-  const buttons = ['LIMPAR', 'DEL', '%', '/', 7, 8, 9, "x", 6, 5, 4, '-', 3, 2, 1, '+', 0, '.', '+/-', '=']
+  const buttons = ['LIMPAR', 'DEL', '%', '/', 7, 8, 9, "x", 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '+/-', '=']
 
   const [currentNumber, setCurrentNumber] = useState("")
   const [lastNumber, setLastNumber] = useState("")
